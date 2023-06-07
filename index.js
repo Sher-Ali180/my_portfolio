ed302b7ff0085158f6bcdea32764c3eef1316718
Sher Ali <sherbat60@gmail.com>
2023-06-07 22:01:01 +0100

    adds about section

---
+++ index.js
@@ -0,0 +1,15 @@
+var tabliks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(link, tabname){
+    console.log(`"${link}"`)
+    for (tablink of tabliks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    document.getElementById(link).classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+};
+window.addEventListener("DOMContentLoaded", (event) => {
+  });
